f1abdf73880e2de5eb924d07b2a1c8b8739f1939
Zsolt Lengyel <zsolt.lengyel.it@gmail.com>
2015-02-27 12:33:32 +0100

    Wiidget template for spring

---
+++ rempi-manager/src/main/java/org/landasource/rempi/manager/core/view/SpringEngine.java
@@ -0,0 +1,15 @@
+package org.landasource.rempi.manager.core.view;
+
+import org.springframework.stereotype.Component;
+
+import com.landasource.wiidget.engine.DefaultEngine;
+
+/**
+ *
+ * @author lzsolt
+ *
+ */
+@Component
+public class SpringEngine extends DefaultEngine {
+
+}
