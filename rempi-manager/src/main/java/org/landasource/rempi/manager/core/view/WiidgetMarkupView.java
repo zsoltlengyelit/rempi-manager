ef4c090cecb133876323e67e71eb7fa4d740a1c4
Zsolt Lengyel <zsolt.lengyel.it@gmail.com>
2015-03-03 08:55:57 +0100

    Use fileloader

--- rempi-manager/src/main/java/org/landasource/rempi/manager/core/view/WiidgetMarkupView.java
+++ rempi-manager/src/main/java/org/landasource/rempi/manager/core/view/WiidgetMarkupView.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextException;
-import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.view.AbstractTemplateView;
 
 import com.landasource.wiidget.Renderer;
@@ -21,65 +20,56 @@ import com.landasource.wiidget.engine.Engine;
 
 public class WiidgetMarkupView extends AbstractTemplateView {
 
-	@Override
-	public boolean checkResource(final Locale locale) throws Exception {
+    @Override
+    public boolean checkResource(final Locale locale) throws Exception {
 
-		final String url = getUrl();
+        final String url = getUrl();
 
-		return getEngine().getConfiguration().getFileLoader().exists(url);
+        return getEngine().getConfiguration().getFileLoader().exists(url);
 
-	}
+    }
 
-	/**
-	 * Invoked at startup.
-	 */
-	@Override
-	protected void initApplicationContext(final ApplicationContext context) {
-		super.initApplicationContext();
-	}
+    /**
+     * Invoked at startup.
+     */
+    @Override
+    protected void initApplicationContext(final ApplicationContext context) {
+        super.initApplicationContext();
+    }
 
-	/**
-	 * Autodetect a MarkupTemplateEngine via the ApplicationContext. Called if a
-	 * MarkupTemplateEngine has not been manually configured.
-	 *
-	 * @param request
-	 * @param model
-	 */
-	protected Engine getEngine() throws BeansException {
+    /**
+     * Autodetect a MarkupTemplateEngine via the ApplicationContext. Called if a
+     * MarkupTemplateEngine has not been manually configured.
+     *
+     * @param request
+     * @param model
+     */
+    protected Engine getEngine() throws BeansException {
 
-		try {
-			return BeanFactoryUtils.beanOfTypeIncludingAncestors(
-					getApplicationContext(), SpringEngine.class, true, false);
-		} catch (final NoSuchBeanDefinitionException ex) {
-			throw new ApplicationContextException(
-					"Expected a single Wiidget engine bean in the current "
-							+ "Servlet web application context or the parent root context: GroovyMarkupConfigurer is "
-							+ "the usual implementation. This bean may have any name.",
-					ex);
-		}
-	}
+        try {
+            return BeanFactoryUtils.beanOfTypeIncludingAncestors(getApplicationContext(), SpringEngine.class, true, false);
+        } catch (final NoSuchBeanDefinitionException ex) {
+            throw new ApplicationContextException("Expected a single Wiidget engine bean in the current "
+                    + "Servlet web application context or the parent root context: GroovyMarkupConfigurer is " + "the usual implementation. This bean may have any name.", ex);
+        }
+    }
 
-	@Override
-	protected void renderMergedTemplateModel(final Map<String, Object> model,
-			final HttpServletRequest request, final HttpServletResponse response)
-			throws Exception {
+    @Override
+    protected void renderMergedTemplateModel(final Map<String, Object> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
 
-		final Engine engine = this.getEngine();
-		engine.getContext().setAll(model);
-		engine.getContext().set("request", request);
-		engine.getContext().set("applicationContext", getApplicationContext());
+        final Engine engine = this.getEngine();
+        engine.getContext().setAll(model);
+        engine.getContext().set("request", request);
+        engine.getContext().set("applicationContext", getApplicationContext());
 
-		final Resource resource = getApplicationContext().getResource(
-				"classpath:" + getUrl());
+        final InputStream inputStream = engine.getConfiguration().getFileLoader().getFile(getUrl());
 
-		final InputStream inputStream = resource.getInputStream();
+        final Renderer renderer = Renderer.create(engine);
+        final String content = renderer.render(inputStream);
 
-		final Renderer renderer = Renderer.create(engine);
-		final String content = renderer.render(inputStream);
-
-		final BufferedWriter writer = new BufferedWriter(response.getWriter());
-		writer.append(content);
-		writer.flush();
-	}
+        final BufferedWriter writer = new BufferedWriter(response.getWriter());
+        writer.append(content);
+        writer.flush();
+    }
 
 }
