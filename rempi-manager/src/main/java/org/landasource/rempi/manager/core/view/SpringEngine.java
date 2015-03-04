0230c488ade5c6706cd67d4604e25cae9e36d459
Zsolt Lengyel <zsolt.lengyel.it@gmail.com>
2015-03-04 11:10:58 +0100

    * Wiidget Default imports  * include login head

--- rempi-manager/src/main/java/org/landasource/rempi/manager/core/view/SpringEngine.java
+++ rempi-manager/src/main/java/org/landasource/rempi/manager/core/view/SpringEngine.java
@@ -1,11 +1,17 @@
 package org.landasource.rempi.manager.core.view;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import com.landasource.wiidget.commons.Include;
+import com.landasource.wiidget.commons.Out;
 import com.landasource.wiidget.engine.DefaultEngine;
 import com.landasource.wiidget.engine.configuration.Configuration;
+import com.landasource.wiidget.library.template.Part;
+import com.landasource.wiidget.library.template.SimplePart;
+import com.landasource.wiidget.library.template.Template;
 
 /**
  *
@@ -14,14 +20,26 @@ import com.landasource.wiidget.engine.configuration.Configuration;
  */
 @Component
 @Scope(org.springframework.web.context.WebApplicationContext.SCOPE_REQUEST)
-public class SpringEngine extends DefaultEngine {
+public class SpringEngine extends DefaultEngine implements InitializingBean {
 
-	@Autowired
-	private SpringWiidgetConfiguration conf;
+    @Autowired
+    private SpringWiidgetConfiguration conf;
 
-	@Override
-	public Configuration getConfiguration() {
-		return conf;
-	}
+    @Override
+    public Configuration getConfiguration() {
+        return conf;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        getConfiguration().addDefaultImport(Out.class);
+
+        // templating
+        getConfiguration().addDefaultImport(Template.class);
+        getConfiguration().addDefaultImport(Part.class);
+        getConfiguration().addDefaultImport(SimplePart.class);
+        getConfiguration().addDefaultImport(Include.class);
+
+    }
 
 }
