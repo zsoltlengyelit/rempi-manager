a75e57266ed1e126307d4a282cccfbc1b6e5a59a
Zsolt Lengyel <zsolt.lengyel.it@gmail.com>
2015-03-19 22:27:12 +0100

    better notification colors

--- rempi-manager/src/main/resources/static/js/pages/device/list.js
+++ rempi-manager/src/main/resources/static/js/pages/device/list.js
@@ -6,20 +6,23 @@ $(function() {
 		stompClient.connect('guest', 'guest', function(frame) {
 			whoami = frame.headers['user-name'];
 			console.log('Connected: ' + frame);
-//			stompClient.subscribe('/user/queue/messages', function(message) {
-//				showMessage(JSON.parse(message.body));
-//			});
+			// stompClient.subscribe('/user/queue/messages', function(message) {
+			// showMessage(JSON.parse(message.body));
+			// });
 			stompClient.subscribe('/devices/checkin', function(message) {
 				var device = JSON.parse(message.body);
 				console.log(device);
-				
-				var status = $('#device-status-' + device.deviceId);
-				
-				status.show();
-				status.css('background', 'green');
-				status.fadeOut(2000);
-				
-				
+
+				if (device.status == 'OK') {
+					var status = $('#device-status-' + device.deviceId);
+
+					status.css('background', 'green');
+					status.css('background', 'yellow');
+					setTimeout(function() {
+						status.css('background', 'green');
+					}, 1000);
+				}
+
 			});
 		});
 	}
