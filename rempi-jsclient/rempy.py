9edc0e118d13201aa278e0e24de01b47cf8cb717
Zsolt Lengyel <zsolt.lengyel.it@gmail.com>
2015-03-25 23:38:42 +0100

    new client

--- rempi-jsclient/rempy.py
+++ rempi-jsclient/rempy.py
@@ -4,42 +4,61 @@ import sys
 import json, requests
 import commands
 
+delay = 8
+requestTimeout = 10
+max_retry_count=10
 host = sys.argv[1]
 port = sys.argv[2]
 serial = commands.getoutput("cat /proc/cpuinfo | grep 'Serial' | awk '{print $3}'")
 print "Serial: " + serial
 
 GPIO.setmode(GPIO.BCM)
+validPins = [2,3,4,14,15,17,18,27,22,23,24,10,9,25,11,8,7,5,6,12,13,19,16,26,20,21]
 
-#GPIO.setup(23, GPIO.OUT)
-for gpioNum in [2, 3, 4, 14, 15, 17, 18, 27, 22, 23, 24, 10, 9, 25, 11, 8, 7, 5, 6, 12, 13, 19, 16, 26, 20, 21]:
-    #	try:
-    GPIO.setup(gpioNum, GPIO.OUT)
-    GPIO.output(gpioNum, False)
-#	except Exception:
-#		pass
 
+def close_pins():
+	for gpioNum in validPins:
+		GPIO.setup(gpioNum, GPIO.OUT)
+		GPIO.output(gpioNum, False)	  	
+	return
+
+def get_bin_state():
+	data = None
+	tries=0
+
+	url = 'http://' + host + ':' + port + '/api/client/'+serial+'/hexstate'
+
+	while (data is None) and (tries < max_retry_count):
+		tries += 1
+		try:
+			resp = requests.get(url=url, timeout = requestTimeout, allow_redirects=False)
+			data = resp.text		
+			data = bin(int(data, 16))[2:].zfill(40)	
+		except requests.exceptions.Timeout:
+			data = None
+			print 'Request time out'
+
+	return data
+
+# main loop
+close_pins()
 
 while True:
 
-    url = 'http://' + host + ':' + port + '/api/client/' + serial + '/state'
-    resp = requests.get(url=url)
-    #data = json.loads(resp.text)
-    data = resp.json()
-    print data['gpio']
+	state = get_bin_state()
+	
+	if(state is None):
+		close_pins()
+		exit() # fail
 
-    for gpioName in data['gpio']:
-        state = data['gpio'][gpioName]
-        gpioNum = int(gpioName[5:])
-        enabled = state['enabled']
+	for gpioNum in validPins:		
+		enabled = True if state[gpioNum] == '1' else False
 
-        GPIO.setup(gpioNum, GPIO.OUT)
-        GPIO.output(gpioNum, enabled)
+		GPIO.setup(gpioNum, GPIO.OUT)
+		GPIO.output(gpioNum, enabled)		
 
-        print "GPIO " + str(gpioNum) + ": " + ('enable' if enabled else 'disabled')
-
-    #	GPIO.output(4, vari)
-    #	vari = not vari
-    time.sleep(2)
+		print "GPIO " + str(gpioNum) + ": " + ('enable' if enabled else 'disabled')
+	
+	time.sleep(delay)
 
 GPIO.cleanup()
--- rempi-manager/src/main/java/org/landasource/rempi/manager/core/schedule/AutowiringSpringBeanJobFactory.java
+++ rempi-manager/src/main/java/org/landasource/rempi/manager/core/schedule/AutowiringSpringBeanJobFactory.java
@@ -1,6 +1,5 @@
 package org.landasource.rempi.manager.core.schedule;
 
-import org.quartz.spi.TriggerFiredBundle;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -8,7 +7,7 @@ import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 
 /**
  * Adds autowiring support to quartz jobs. Created by david on 2015-01-20.
- * 
+ *
  * @see https://gist.github.com/jelies/5085593
  */
 public final class AutowiringSpringBeanJobFactory extends SpringBeanJobFactory implements ApplicationContextAware {
@@ -20,10 +19,11 @@ public final class AutowiringSpringBeanJobFactory extends SpringBeanJobFactory i
 		beanFactory = context.getAutowireCapableBeanFactory();
 	}
 
-	@Override
-	protected Object createJobInstance(final TriggerFiredBundle bundle) throws Exception {
-		final Object job = super.createJobInstance(bundle);
-		beanFactory.autowireBean(job);
-		return job;
-	}
+	// @Override
+	// protected Object createJobInstance(final TriggerFiredBundle bundle) throws
+	// Exception {
+	// final Object job = super.createJobInstance(bundle);
+	// beanFactory.autowireBean(job);
+	// return job;
+	// }
 }
