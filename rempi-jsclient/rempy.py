82b98ca845fb615ab81e110d8fa949c7fe29b591
Zsolt Lengyel <zsolt.lengyel.it@gmail.com>
2015-03-30 20:51:50 +0200

    fine client

--- rempi-jsclient/rempy.py
+++ rempi-jsclient/rempy.py
@@ -7,6 +7,7 @@ import commands
 delay = 8
 requestTimeout = 10
 max_retry_count=10
+retry_sleep=5
 host = sys.argv[1]
 port = sys.argv[2]
 serial = commands.getoutput("cat /proc/cpuinfo | grep 'Serial' | awk '{print $3}'")
@@ -15,7 +16,7 @@ print "Serial: " + serial
 GPIO.setmode(GPIO.BCM)
 validPins = [2,3,4,14,15,17,18,27,22,23,24,10,9,25,11,8,7,5,6,12,13,19,16,26,20,21]
 
-time.sleep(20) # wait for 3g
+#time.sleep(5) # wait for 3g
 
 def close_pins():
 	for gpioNum in validPins:
@@ -34,10 +35,22 @@ def get_bin_state():
 		try:
 			resp = requests.get(url=url, timeout = requestTimeout, allow_redirects=False)
 			data = resp.text		
-			data = bin(int(data, 16))[2:].zfill(40)	
+			if len(data) == 0:
+				print "Empty response"
+				data = None
+				time.sleep(retry_sleep)
+			else:
+				data = bin(int(data, 16))[2:].zfill(40)	
 		except requests.exceptions.Timeout:
 			data = None
+			time.sleep(retry_sleep)
 			print 'Request time out'
+		except requests.exceptions.ConnectionError:
+			data = None
+			time.sleep(retry_sleep)
+		except:
+			time.sleep(retry_sleep)
+			print "Unhandled error: %s" % (sys.exc_info()[0])
 
 	return data
 
@@ -45,21 +58,25 @@ def get_bin_state():
 close_pins()
 
 while True:
+	try:
 
-	state = get_bin_state()
-	
-	if(state is None):
+		state = get_bin_state()
+		
+		if(state is None):
+			close_pins()
+			exit() # fail
+
+		for gpioNum in validPins:		
+			enabled = True if state[gpioNum] == '1' else False
+
+			GPIO.setup(gpioNum, GPIO.OUT)
+			GPIO.output(gpioNum, enabled)		
+
+			print "GPIO " + str(gpioNum) + ": " + ('enable' if enabled else 'disabled')
+		
+		time.sleep(delay)
+	except:
 		close_pins()
-		exit() # fail
 
-	for gpioNum in validPins:		
-		enabled = True if state[gpioNum] == '1' else False
-
-		GPIO.setup(gpioNum, GPIO.OUT)
-		GPIO.output(gpioNum, enabled)		
-
-		print "GPIO " + str(gpioNum) + ": " + ('enable' if enabled else 'disabled')
-	
-	time.sleep(delay)
-
+close_pins();
 GPIO.cleanup()
